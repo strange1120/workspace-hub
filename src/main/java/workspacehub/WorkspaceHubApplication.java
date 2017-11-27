98957a0e9d29575a140164d7d278894ea1ccc6cc
Sarah Strange <strange1120@gmail.com>
2017-11-27 11:22:49 -0500

    base commit

---
+++ src/main/java/workspacehub/WorkspaceHubApplication.java
@@ -0,0 +1,12 @@
+package workspacehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceHubApplication.class, args);
+	}
+}
